bef3c830c275ee986be779c8eba32e5a2ba3e95b
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2023-07-19 18:11:18 +0200

    Second version of hexador working, with subroutines and buffer with variable capacity (beffore locked to 16 bytes)

--- hexador/hexador2.s
+++ hexador/hexador2.s
@@ -6,13 +6,13 @@
 ;        ld -o hexador hexador.o
 section .data                           ; Initialized data
 ; Template line for hex characters
-DumpLin: db " 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+DumpLin: db " 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00", 0x09
 DUMPLEN: equ $-DumpLin 
 ; Template line for ascii characters
-AsciiLin: db "|................|",10
-ASCIILEN: equ $-AsciiLin
+AsciiLin: db "|................|",10,0
+ASCIILEN: equ $-AsciiLin - 1
 ; Hex characters 
-HexDigits: db "0123456789ABCDEF"
+HexDigits: db "0123456789ABCDEF",0
 ; Translation table from hex to ascii characters
 ; All valid ascii characters (lower than 128) are identified with themselves
 ; and all others values are mapped to the dot char 2Eh
@@ -48,7 +48,7 @@ section .text                           ; Actual code
 ClearLine:
         push rax
         push rdx                        ; Save the gp registers that will be used
-        mov rdx, 0Fh                    ; We have to clean 16=Fh spaces
+        mov rdx, 0x0F                   ; We have to clean 16=Fh spaces
         .loop:
                 mov rax, 0              ; Empty byte that we'll write 
                 call DumpChar 
@@ -60,7 +60,7 @@ ClearLine:
 ;--------------------------------------------------------------------------------
 ; DumpChar: Clear dump line and ascii line (resets them to original values)
 ; In:
-;        rax: value to write
+;        al: value to write
 ;        rdx: pointer (0 to 15) of where to dump the byte
 ; Returns: Nothing
 ; Modifies: rax, AsciiLin, DumpLin
@@ -69,15 +69,16 @@ DumpChar:
         push rbx                        ; We push rbx to stack as we'll use it
         push rdi                        ; same as above
 ; First we will print ascii char because dumping hex value is destructive
+        and rax, 0xFF                   ; We clean all subbytes but al
         mov bl, byte [LUTable + rax]    ; We go through look up table
         mov [AsciiLin + 1 + rdx], bl    ; We need to + 1 as first character is '|'
 ; Now we'll generate offset for dumping hex
         lea rdi, [rdx * 2 + rdx]        ; In rdi we will store actual offset i.e. rdi = rdx * 3
 
         mov rbx, rax                    ; Restore rbx as byte to dump        
-        and rax, 00000000000000F0h      ; Mask out high nybble
+        and rax, 0xF0                   ; Mask out high nybble
         shr rax, 4                      ; Shift 4 to right so high nybble becomes valid in al
-        and rbx, 000000000000000Fh      ; Mask out low nybble
+        and rbx, 0x0F                   ; Mask out low nybble
         mov al, byte [HexDigits + rax]  ; Now in rax we have the actual char for hex value
         mov bl, byte [HexDigits + rbx]  ; Now in rbx we have the actual char for hex value
 
@@ -97,7 +98,7 @@ PrintLine:
         push rax 
         push rdi
         push rsi
-        push rdx                        ; Stack shenanigans
+        push rdx                        ; Save registers
         mov rax, 1                      ; Code for sys_write
         mov rdi, 1                      ; Code for stdout
         mov rsi, DumpLin                ; startpoint of line to write
@@ -106,26 +107,65 @@ PrintLine:
         pop rdx
         pop rsi
         pop rdi
-        pop rax                         ; Stack shenanigans
+        pop rax                         ; Restore registers
         ret
+;--------------------------------------------------------------------------------
+; FillBuffer: Fill buffer with data from stdin
+; In: Nothing
+; Returns: #no of bytes read in EBP
+; Modifies: RAX, R8, Buff
+; Description:
+;        Fills the Buff with data from stdin (BUFFLEN bytes) using sys_read
+;        RAX returns the number of bytes read, so if it's 0 we reached EOF
+;        a negative value on EBP indicates error so program must test it
+;        R8 is reseted because buffer has to be read from 0 to BUFFLEN - 1
+; Calls: sys_read
+FillBuffer:
+        push rsi
+        push rdi
+        push rdx                        ; Save registers
+        mov rax, 0                      ; code for sys_read
+        mov rdi, 0                      ; Code for stdin file descriptor
+        mov rsi, Buff                   ; Pointer to buffer
+        mov rdx, BUFFLEN                ; How many bytes to read
+        mov r8, 0                       ; Reset counter
+        syscall
+        pop rdx
+        pop rdi
+        pop rsi                         ; Restore registers
+        ret
+
 global _start
 _start:
-        mov rax, 61h        
-        mov rdx, 0
-        call DumpChar
-        mov rax, 62h        
-        mov rdx, 1
-        call DumpChar
-        mov rax, 64h        
-        mov rdx, 2
-        call DumpChar
-        mov rax, 10h        
-        mov rdx, 4
-        call DumpChar
-        call PrintLine
-        call ClearLine
-        call PrintLine
+        call FillBuffer                 ; We fill the buffer with data from stdin, now R8 = 0
+        cmp rax, 0                      ; We test rax 
+        jbe Exit                        ; If rax <= We reached EOF or encoutered an error
+        mov rdx, 0                      ; Counter for no of chars processed in a line 
+        mov r9, rax                     ; R9 now contains number of bytes in buffer
+Scan: 
+        movzx rax, byte [Buff + r8]     ; Now RAX contains the byte to dump (we move with zero expansion)
+; As RDX already contains no of byte to process we can directly call DumpChar
+        call DumpChar                   ; RAX is lost 
+; Now we have to check if it's necessary to print line or load the buffer again        
+        inc r8                          ; Increment buffer pointer
+        cmp r8, r9                      ; Compare buffer pointer with no of bytes in buffer
+        jb .testLine                    ; If R8 < RDX we still have buffer to process
+                call FillBuffer         ; otherwise we refill the buffer
+                cmp rax, 0              ; If buffer reached EOF we can finish the program
+                jb Exit                 ; Exit without printing last line because we errored
+                je Done                 ; If we simply reached EOF we're done without issues
+
+        .testLine:
+                                        ; If RDI > 16 we have to print line and clear it
+                inc rdx
+                and rdx, 0x0F           ; 16 = 10h so this will give 0 in such case, otherwise it doesn't affect it 
+                jnz Scan                ; If it's not zero we can scan next byte without issues
+                call PrintLine          ; Otherwise we print the line and clear it afterwards
+                call ClearLine
+                jmp Scan
+Done: 
+        call PrintLine                  ; Finish printing remaining data
 Exit:
-        mov rax, 3Ch                    ; Code for sys_exit
-        mov rdi, 0                      ; Return value for program
+        mov rdi, rax                    ; Return value for program
+        mov rax, 0x3C                   ; Code for sys_exit
         syscall
